b84ee0757fddbd78afc9b13799406efcb3e169c2
PramodKumara M <114072285+PramodKumarShimoga@users.noreply.github.com>
2023-01-20 11:18:53 +0530

    Delete firstAidspringQuarkusValidation directory

--- firstAidspringQuarkusValidation/repo/FirstaidRepo.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.firstAidspringQuarkusValidation.repo;
-
-import com.xworkz.firstAidspringQuarkusValidation.dto.MissileDTO;
-
-public interface FirstaidRepo {
-	
-	boolean save(MissileDTO fdto);
-
-}
--- firstAidspringQuarkusValidation/service/FirstaidService.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.firstAidspringQuarkusValidation.service;
-
-import com.xworkz.firstAidspringQuarkusValidation.dto.MissileDTO;
-
-public interface FirstaidService {
-	
-	boolean saveAndValidate(MissileDTO vfdto);
-
-}
